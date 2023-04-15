67aad406e4dcf1598f66704b0990da4ce4174a48
Nick Bevers <nick.bevers9@gmail.com>
2023-04-15 13:15:39 +0200

    initial commit with mapping component

--- src/components/mappingCanvas.vue
+++ src/components/mappingCanvas.vue
@@ -3,7 +3,6 @@
 import { onMounted, onBeforeMount, toRefs } from "vue";
 import * as poseDetection from "@tensorflow-models/pose-detection";
 import "@tensorflow/tfjs-backend-webgl";
-import { drawCanvas } from "../functions/clothesMapping/drawCanvas";
 
 //  Define the props that the component will receive
 const props = defineProps<{ imgSrc: string }>();
@@ -25,8 +24,8 @@ let detector: poseDetection.PoseDetector;
 let stopDetecting = false;
 let clothing = true;
 let video: HTMLVideoElement;
-const leftShoulder: { x: number; y: number } = { x: 0, y: 0 };
-const rightShoulder: { x: number; y: number } = { x: 0, y: 0 };
+let leftShoulder: { x: number; y: number } = { x: 0, y: 0 };
+let rightShoulder: { x: number; y: number } = { x: 0, y: 0 };
 
 // Get an instance of the detector before the component is mounted to reduce the loading time
 onBeforeMount(async () => {
@@ -38,7 +37,6 @@ onMounted(async () => {
     if ("mediaDevices" in navigator && navigator.mediaDevices.getUserMedia) {
         navigator.mediaDevices
             .getUserMedia({
-                // audio is not needed so can be set to false
                 video: true,
                 audio: false,
             })
@@ -54,20 +52,7 @@ onMounted(async () => {
                     width = video!.width;
                     height = video!.height;
                     // start the drawing of the canvas when the video is loaded
-                    drawCanvas(
-                        document.querySelector(".canvas--map") as HTMLCanvasElement,
-                        video,
-                        fps,
-                        stopDetecting,
-                        width,
-                        height,
-                        detector,
-                        clothing,
-                        img,
-                        verticalOffset,
-                        leftShoulder,
-                        rightShoulder
-                    );
+                    drawCanvas();
                 };
             })
             .catch((err) => {
@@ -78,31 +63,114 @@ onMounted(async () => {
     }
 });
 
-// functions that can be called from the template to start and stop the detection
+// draw the canvas and get the poses each frame
+const drawCanvas = () => {
+    // get the canvas and set its width and height (also get the context)
+    const canvas: HTMLCanvasElement = document.querySelector(".canvas--map") as HTMLCanvasElement;
+    const ctx: CanvasRenderingContext2D = canvas.getContext("2d") as CanvasRenderingContext2D;
+    canvas.width = width;
+    canvas.height = height;
+
+    // draw the video on the canvas, but only if the video is not paused or ended
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    if (video.paused || video.ended) return;
+
+    // get the poses and draw them on the canvas
+    getPoses();
+
+    // use requestAnimationFrame to draw the canvas again (1000 / fps => fps times per second)
+    if (!stopDetecting) {
+        setTimeout(() => {
+            requestAnimationFrame(drawCanvas);
+        }, 1000 / fps);
+    }
+};
+
+// get the poses and draw them on the canvas
+const getPoses = async () => {
+    // if the detector is not undefined, get the poses and draw them on the canvas
+    if (detector) {
+        // get the poses from the detector
+        const poses = await detector.estimatePoses(video!);
+
+        // if there are no poses, return
+        if (poses.length === 0) {
+            return;
+        }
+
+        // get the keypoints from the first pose and draw them on the canvas
+        const keypoints = poses[0].keypoints;
+        drawKeypoints(keypoints);
+    } else {
+        return;
+    }
+};
+
+// draw the keypoints on the canvas and call the showTshirt function if clothing is true
+const drawKeypoints = (keypoints: poseDetection.Keypoint[]) => {
+    const canvas: HTMLCanvasElement = document.querySelector(".canvas--clothes") as HTMLCanvasElement;
+    const ctx: CanvasRenderingContext2D = canvas.getContext("2d") as CanvasRenderingContext2D;
+    canvas.width = width;
+    canvas.height = height;
+
+    //  filter out the keypoints with a score lower than 0.35 (the higher the score, the more confident the model is)
+    const newKeypoints = keypoints.filter((keypoint) => keypoint.score! > 0.35);
+    let shoulderDistance = 0;
+
+    // loop through the keypoints set the shoulderDistance
+    newKeypoints.forEach((keypoint) => {
+        ctx.beginPath();
+        ctx.arc(keypoint.x, keypoint.y, 4, 0, 2 * Math.PI);
+        if (keypoint.name === "right_shoulder") {
+            rightShoulder = { x: keypoint.x, y: keypoint.y };
+        } else if (keypoint.name === "left_shoulder") {
+            leftShoulder = { x: keypoint.x, y: keypoint.y };
+            shoulderDistance = Math.sqrt(
+                Math.pow(keypoint.x - rightShoulder.x, 2) + Math.pow(keypoint.y - rightShoulder.y, 2)
+            );
+        }
+    });
+
+    // if clothing is true, call the showTshirt function
+    if (clothing) {
+        showClothes(shoulderDistance);
+    }
+};
+
+// draw the tshirt on the canvas
+const showClothes = (distance: number) => {
+    // get the canvas and context and calculate the scale and angle
+    const canvas: HTMLCanvasElement = document.querySelector(".canvas--clothes") as HTMLCanvasElement;
+    const ctx: CanvasRenderingContext2D = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+    // calculate the scale and angle
+    const scale = (distance / img.width) * 2;
+    const angle = Math.atan2(rightShoulder.y - leftShoulder.y, rightShoulder.x - leftShoulder.x) + Math.PI;
+
+    // draw the clothng piece on the canvas (tshirt) and rotate it to the correct angle
+    ctx.translate(leftShoulder.x, leftShoulder.y);
+    ctx.rotate(angle);
+
+    // calculate the new width and height and draw the image on the canvas
+    const newWidth = img.width * scale;
+    const newHeight = img.height * scale;
+    ctx.drawImage(img, -(canvas.width * scale) - 40, -verticalOffset, newWidth, newHeight);
+
+    // reset the canvas to its original state
+    ctx.rotate(-angle);
+    ctx.translate(-leftShoulder.x, -leftShoulder.y);
+};
+
+// functions that can be called from the template to start and stop the detection (development only)
 const startDetect = () => {
     stopDetecting = false;
     video.play();
-    drawCanvas(
-        document.querySelector(".canvas--map") as HTMLCanvasElement,
-        video,
-        fps,
-        stopDetecting,
-        width,
-        height,
-        detector,
-        clothing,
-        img,
-        verticalOffset,
-        leftShoulder,
-        rightShoulder
-    );
+    drawCanvas();
 };
-
 const stopDetect = () => {
     stopDetecting = true;
     video.pause();
 };
-
 const toggleShirt = () => {
     clothing = !clothing;
 };
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import Camera from "simple-vue-camera";
 
-createApp(App).component("camera", Camera).mount("#app");
+createApp(App).mount("#app");
