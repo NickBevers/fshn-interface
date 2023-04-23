86bfa573b1bed714cb5d04de6e27f74f21075f9c
MarieSerroyen <marie.serroyen@gmail.com>
2023-04-23 10:14:10 +0200

    feat: add detial page

---
+++ src/DetailPage.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+    // import components
+    import Navigation from './components/navComponent.vue';
+</script>
+
+<template>
+    <Navigation/>
+
+    <div class="content">
+
+    </div>
+
+
+</template>
+
+<style scoped>
+    .content {
+        margin-top: 250px;
+    }
+</style>
---
+++ src/detail.ts
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import "./style.css";
+import Detail from "./DetailPage.vue";
+
+createApp(Detail).mount("#detail");
