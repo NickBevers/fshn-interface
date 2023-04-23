e2b5686d77ead82369a18fb631e5944a71557114
MarieSerroyen <marie.serroyen@gmail.com>
2023-04-23 14:09:21 +0200

    style: detail screen

--- src/components/clothingDetail.vue
+++ src/components/clothingDetail.vue
@@ -16,39 +16,42 @@
 
         <div class="details">
 
-            <div class="">
-                <p>Top with bare shoulders</p>
-                <a href="">
-                    Try On <span>AR</span>
-                </a>
-            </div>
+            <div class="item_info">
+                <div class="top_info">
+                    <p class="item_name">Top with bare shoulders</p>
+                    <a class="yellow_btn" href="index.html">
+                        Try On 
+                        <img class="icon" src="../assets/AR_icon.svg" alt="AR icon">
+                    </a>
+                </div>
 
-            <div>
-                <div>
-                    <p>Color</p>
-                    <select id="color" name="color" >
-                        <option value="black">Black</option>
-                        <option value="white">White</option>
-                        <option value="red">red</option>
-                    </select>
+                <div class="item_preference">
+                    <div class="dropdown">
+                        <p class="label">Color</p>
+                        <select id="color" name="color" >
+                            <option value="black">Black</option>
+                            <option value="white">White</option>
+                            <option value="red">Red</option>
+                        </select>
+                    </div>
 
-                    <p>Size</p>
-                    <select id="size" name="size" >
-                        <option value="s">S</option>
-                        <option value="m">M</option>
-                        <option value="l">L</option>
-                    </select>
+                    <div class="dropdown">
+                        <p class="label">Size</p>
+                        <select id="size" name="size" >
+                            <option value="s">S</option>
+                            <option value="m">M</option>
+                            <option value="l">L</option>
+                        </select>
+                    </div>
                 </div>
             </div>
 
-            <hr>
-
-            <div>
+            <div class="item_desc">
                 <p class="description">
                     Lorem ipsum dolor sit amet, consectetur odio adipiscing elit.
                     Nunc vulputate libero et velit elit interdum, ac aliquet odio mattis.
                 </p>
-                <a href="cart.html">Add to Cart</a>
+                <a class="black_btn" href="cart.html">Add to Cart</a>
             </div>
         </div>
     </div>
@@ -56,6 +59,13 @@
 </template>
 
 <style scoped>
+    p {
+        color: white;
+    }
+
+    a {
+        color: white;
+    }
     .container{
         padding: 0 40px;
     }
@@ -86,6 +96,114 @@
         width: 25%;
         height: 100%;
         object-fit: cover;
+    }
+
+    .details{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding-top: 20px;
+        /*padding: 20px 150px;*/
+    }
+    .top_info {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        gap: 550px;
+    }
+
+    .item_name {
+        font-size: 3rem;
+        font-weight: 400;
+        padding-bottom: 20px;
+        text-transform: uppercase;
+    }
+
+    .yellow_btn{
+        background-color: #FBC67A;
+        color: black;
+        border: none;
+        padding: 10px 20px;
+        font-size: 1.5rem;
+        font-weight: 400;
+        cursor: pointer;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        gap: 10px;
+    }
+
+    .icon {
+        width: 40px;
+        height: 40px;
+    }
+
+    .item_preference{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        padding-top: 20px;
+    }
+
+    .dropdown {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        gap: 50px;
+    }
+
+    .label{
+        font-size: 1.5rem;
+        font-weight: 400;
+        padding-bottom: 10px;
+        color: slategrey;
+    }
+
+    select {
+        color: white;
+        background-color: transparent;
+        border: none;
+        font-size: 1.5rem;
+        padding: 0 3em 0 0;
+        outline: none;
+        padding-bottom: 10px;
+    }
+
+    option {
+        color: white;
+        background-color: black;
+        border: none;
+        font-size: 1.5rem;
+        margin-right: 20px;
+    }
+
+    .item_desc{
+        border-top: solid 1px slategrey;
+        margin: 0 200px;
+    }
+
+    .description{
+        font-size: 2rem;
+        font-weight: 300;
+        padding: 20px 0;
+        line-height: normal;
+    }
+
+    .black_btn {
+        display: block;
+        width: 100%;
+        border: solid 1px white;
+        background-color: transparent;
+        padding: 14px 5px;
+        font-size: 1.2rem;
+        cursor: pointer;
+        text-align: center;
+        margin-bottom: 50px;
     }
 
 </style>
