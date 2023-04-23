8ea86e2bdb54be4d59d948bc1dd22cbb3f948ae1
MarieSerroyen <marie.serroyen@gmail.com>
2023-04-23 16:01:43 +0200

    feat: add category page

---
+++ src/CategoryPage.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+    // import components
+    import Navigation from './components/navComponent.vue';
+    import Category from './components/categoryComponent.vue';
+</script>
+
+<template>
+    <Navigation/>
+
+    <div class="content">
+        <Category/>
+    </div>
+
+
+</template>
+
+<style scoped>
+    .content {
+        margin-top: 250px;
+    }
+</style>
---
+++ src/category.ts
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import "./style.css";
+import Category from "./CategoryPage.vue";
+
+createApp(Category).mount("#category");
