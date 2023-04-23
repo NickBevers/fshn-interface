bd95c3351e5c9e776c5b055eed6fd924ee21340c
MarieSerroyen <marie.serroyen@gmail.com>
2023-04-23 09:43:20 +0200

    feat: add clothing items

--- src/CatalogPage.vue
+++ src/CatalogPage.vue
@@ -1,14 +1,29 @@
 <script lang="ts" setup>
+    // import components
     import Navigation from './components/navComponent.vue';
     import SubCategory from './components/subCategory.vue';
+    import Items from './components/clothingItems.vue';
 </script>
 
 <template>
     <Navigation/>
-    <SubCategory/>
+
+    <div class="content">
+        <SubCategory/>
+        <div class="items">
+            <Items/>
+        </div> 
+    </div>
+
 
 </template>
 
 <style scoped>
+    .content {
+        margin-top: 250px;
+    }
 
+    .items {
+        margin-top: 100px;
+    }
 </style>
---
+++ src/components/clothingItems.vue
@@ -0,0 +1,47 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div class="container">
+        <div class="clothing-item">
+            <img class="clothing-item-img" src="../assets/red_blouse.jpg" alt="">
+        </div>
+        <div class="clothing-item">
+            <img class="clothing-item-img" src="../assets/glitter_blouse.jpg" alt="">
+        </div>
+        <div class="clothing-item">
+            <img class="clothing-item-img" src="../assets/pink_top.jpg" alt="">
+        </div>
+        <div class="clothing-item">
+            <img class="clothing-item-img" src="../assets/black_top.jpg" alt="">
+        </div>
+        <div class="clothing-item">
+            <img class="clothing-item-img" src="../assets/flower_top.jpg" alt="">
+        </div>
+        <div class="clothing-item">
+            <img class="clothing-item-img" src="../assets/snake_top.jpg" alt="">
+        </div>
+        <div class="clothing-item">
+            <img class="clothing-item-img" src="../assets/black_piece.jpg" alt="">
+        </div>
+        <div class="clothing-item">
+            <img class="clothing-item-img" src="../assets/white_top.jpg" alt="">
+        </div>      
+    </div>
+</template>
+
+<style scoped>
+    .container {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        padding-left: 100px;
+    }
+
+    .clothing-item {
+        padding-bottom: 90px;
+    }
+    .clothing-item-img {
+        width: 90%;
+    }
+</style>
