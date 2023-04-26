97229fc9cdd9a7dc7acecf0cf78644ca80b635dd
NickBevers <nick.bevers9@gmail.com>
2023-04-26 10:38:00 +0200

    feat: add multiple tshirts for mapping (color variations)

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import MappingCanvas from "./components/mappingCanvas.vue";
     <Navigation/>
     <div class="content">
         <!-- To implement it in any page, use the component and give it 1 prop (the path to the image it has to map.) -->
-        <MappingCanvas :img-src="'/src/assets/tshitTest.png'" />
+        <MappingCanvas :img-src="'/src/assets/mapping/tshirt-black.png'" />
     </div>
 
 </template>
