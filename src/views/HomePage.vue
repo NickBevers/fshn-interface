717618ea0facd07268cb3b04676f68fd59229336
Nick Bevers <nick.bevers9@gmail.com>
2023-05-30 20:45:49 +0200

    refactor: change links for images

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -22,11 +22,11 @@ const sweater: {type: string, verticalOffset: number, horizontalOffset: number} 
         </div>
 
         <!-- To implement it in any page, use the component and give it 4 props (the path to the image it has to map, the type of image (top or bottom), the verticalOfsset to the left shoulder and the horizontalOffset to the left shoulder) -->
-        <MappingCanvas :img-src="'/src/assets/mapping/pull.png'" :place="sweater.type" :vertical-offset="sweater.verticalOffset" :horizontal-offset="sweater.horizontalOffset" />
-        <!-- <MappingCanvas :img-src="'/src/assets/mapping/pull-S.png'" :place="sweater.type" :vertical-offset="sweater.verticalOffset" :horizontal-offset="sweater.horizontalOffset" /> -->
+        <MappingCanvas :img-src="'https://files.nickbevers.be/pull.png'" :place="sweater.type" :vertical-offset="sweater.verticalOffset" :horizontal-offset="sweater.horizontalOffset" />
+        <!-- <MappingCanvas :img-src="'https://files.nickbevers.be/pull-S.png'" :place="sweater.type" :vertical-offset="sweater.verticalOffset" :horizontal-offset="sweater.horizontalOffset" /> -->
 
         <!-- Pants -->
-        <!-- <MappingCanvas :img-src="'/src/assets/mapping/pants.png'" :place="pants.type" :vertical-offset="pants.verticalOffset" :horizontal-offset="pants.horizontalOffset" /> -->
+        <!-- <MappingCanvas :img-src="'https://files.nickbevers.be/pants.png'" :place="pants.type" :vertical-offset="pants.verticalOffset" :horizontal-offset="pants.horizontalOffset" /> -->
     </div>
 
 </template>
